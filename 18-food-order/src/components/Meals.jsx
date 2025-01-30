0ecda9313f3892335a61c3e5dae7107f9f8fe2f6
Daniel Lee <iphone.work.kt@gmail.com>
2025-01-29 21:39:37 -0700

    Fifteenth project: food order app fin

--- 18-food-order/src/components/Meals.jsx
+++ 18-food-order/src/components/Meals.jsx
@@ -1,21 +1,26 @@
-import { useState, useEffect } from 'react';
+import useHttp from '../hooks/useHttp';
 import MealItem from './MealItem';
+import Error from './Error';
+
+const requestConfig = {};
+// if you put {} instead of requestConfig, it will fall into infinite loop.
+// because js is keep creating a new object.
 
 export default function Meals() {
-    const [loadedMeals, setLoadedMeals] = useState([]);
+    const { data: loadedMeals, isLoading, error } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
-            if (!response.ok) {
-                // ...
-            }
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
-        fetchMeals();
-    }, [])
 
+    if (isLoading) {
+        return <p className='center'>Fetching meals...</p>;
+    }
+
+    if (error) {
+        return <Error title='Failed to fetch meals' message={error} />;
+    }
+
+    // if (!data){
+    //     return <p>No meals found.</p>;
+    // }
 
     return (
         <ul id="meals">

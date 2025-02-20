923fc5bc83d96c076a1b1445dcc44f82a5f484d5
Daniel Lee <iphone.work.kt@gmail.com>
2025-02-19 17:39:26 -0700

    Sixteenth project: redux basic - with redux toolkit

--- 19b-redux-basic/src/store/index.js
+++ 19b-redux-basic/src/store/index.js
@@ -1,42 +1,40 @@
-// import { createSlice, configureStore } from '@reduxjs/toolkit'
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit'
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        };
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+            // this works as an immutable way
+            // internally copies all other properties
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        // if we need some data then we can put action
+        increase(state, action) {
+            state.counter = state.counter + action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        },
     }
-
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        };
-    }
-
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        };
-    }
-
-    if (action.type === 'toggle') {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-    }
-
-    return state;
-}
+});
 
 
-const store = createStore(counterReducer);
+// const store = createStore(counterReducer);
+// configureStore allows merging multiple reducers
+// need to pass an object
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
+
+// will create action objects for us
+// no identifier is needed!
+export const counterActions = counterSlice.actions
 
 export default store;
 
